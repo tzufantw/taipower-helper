e54aea704e85fedce28d85e4a279b20d760a6c16
tzufantw <tzufan.tw@gmail.com>
2026-07-20 00:08:36 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="tph-team-v1";
-const ASSETS=["./","./index.html","./style.css","./app.js","./manifest.json"];
+const CACHE="tph-team-v2";
+const ASSETS=["./","./index.html","./style.css","./app.js?v=31","./manifest.json"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
