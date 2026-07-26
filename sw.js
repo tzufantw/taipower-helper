b7bc2a3e4816bafe34a38be033712d7024b3f050
tzufantw <tzufan.tw@gmail.com>
2026-07-26 15:26:51 +0800

    Publish V32 faster QR scanning

--- sw.js
+++ sw.js
@@ -1,4 +1,9 @@
-const CACHE="tph-team-v2";
-const ASSETS=["./","./index.html","./style.css","./app.js?v=31","./manifest.json"];
+const CACHE="tph-team-v32";
+const ASSETS=["./","./index.html","./style.css","./app.js?v=32","./manifest.json"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("activate",e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(
+    keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))
+  ))
+));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
