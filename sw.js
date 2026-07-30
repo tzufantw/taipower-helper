0499627fadb571f3485aa3805c7cb9f4b675ea4d
tzufantw <tzufan.tw@gmail.com>
2026-07-30 23:34:07 +0800

    Refresh V43 offline cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="tph-team-v42-0";
-const ASSETS=["./","./index.html","./style.css?v=41.0","./app.js?v=42.0","./manifest.json"];
+const CACHE="tph-team-v43-0";
+const ASSETS=["./","./index.html","./style.css?v=43.0","./app.js?v=43.0","./manifest.json"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(
   caches.keys().then(keys=>Promise.all(
